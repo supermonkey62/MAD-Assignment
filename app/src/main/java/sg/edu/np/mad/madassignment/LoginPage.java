357a880f7a55d888bd4b4519efe7aad2979187c1
Kenneth <kennethbwwu@gmail.com>
2023-06-14 17:32:58 +0800

    update login page stuff

--- app/src/main/java/sg/edu/np/mad/madassignment/LoginPage.java
+++ app/src/main/java/sg/edu/np/mad/madassignment/LoginPage.java
@@ -2,50 +2,13 @@ package sg.edu.np.mad.madassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginPage extends AppCompatActivity {
 
-    final String TITLE = "Main Activity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, timer.class);
-        startActivity(intent);
-
-        Log.v(TITLE, "On Create!");
+        setContentView(R.layout.activity_login_page);
     }
-    @Override
-    protected void onStart(){
-        super.onStart();
-        Log.v(TITLE, "On Start!");
-    }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-        Log.v(TITLE, "On Pause!");
-    }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-        Log.v(TITLE, "On Resume!");
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-        Log.v(TITLE, "On Stop!");
-    }
-
-    @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        Log.v(TITLE, "On Destroy!");
-    }
-
 }
