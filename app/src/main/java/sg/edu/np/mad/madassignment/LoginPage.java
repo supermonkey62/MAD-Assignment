51dbcb06a5209b88e425fcc05274bd013d51323e
Ernqi <S10243067@connect.np.edu.sg>
2023-06-15 01:11:09 +0800

    made lanscapes of timers and colored them

--- app/src/main/java/sg/edu/np/mad/madassignment/LoginPage.java
+++ app/src/main/java/sg/edu/np/mad/madassignment/LoginPage.java
@@ -1,7 +1,5 @@
 package sg.edu.np.mad.madassignment;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
--- app/src/main/java/sg/edu/np/mad/madassignment/MainPage.java
+++ app/src/main/java/sg/edu/np/mad/madassignment/MainPage.java
@@ -2,6 +2,7 @@ package sg.edu.np.mad.madassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +12,15 @@ import android.widget.Toast;
 public class MainPage extends AppCompatActivity {
 
     Button pomodorotimer;
+    Button normaltimer;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
 
         pomodorotimer = findViewById(R.id.pomobutton);
+        normaltimer = findViewById(R.id.NormalTimerBttn);
 
         pomodorotimer.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +28,15 @@ public class MainPage extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), " Entering Pomodoro Timer", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(MainPage.this, PomodoroTimer.class);
                 startActivity(intent);
+            }
+        });
+
+        normaltimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainPage.this,"Entering Timer",Toast.LENGTH_SHORT).show();
+                Intent intent2 = new Intent(MainPage.this, timer.class);
+                startActivity(intent2);
             }
         });
     }
