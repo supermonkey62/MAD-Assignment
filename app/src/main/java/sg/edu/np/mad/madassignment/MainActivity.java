f49422f39dedeaf70b6c5eddb22ff72d0be9f88a
Ernqi <S10243067@connect.np.edu.sg>
2023-06-14 17:08:21 +0800

    Created an interactive pomodoro timer

--- app/src/main/java/sg/edu/np/mad/madassignment/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madassignment/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       
+
         Log.v(TITLE, "On Create!");
     }
     @Override
