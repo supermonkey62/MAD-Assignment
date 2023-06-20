d965a6189ebae8bf9bbd32fc3d0ab900fc8228c7
Ernqi <S10243067@connect.np.edu.sg>
2023-06-20 10:26:53 +0800

    fixed some layout issues

--- app/src/main/java/sg/edu/np/mad/madassignment/SplashActivity.java
+++ app/src/main/java/sg/edu/np/mad/madassignment/SplashActivity.java
@@ -11,7 +11,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        getSupportActionBar().hide();
+//        getSupportActionBar().hide();
 
         final Intent i = new Intent(SplashActivity.this,LoginPage.class);
         new Handler().postDelayed(new Runnable() {
