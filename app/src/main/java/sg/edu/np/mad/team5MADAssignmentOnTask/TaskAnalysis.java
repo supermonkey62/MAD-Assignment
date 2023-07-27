19532c305bb7cd78c77c8dfa02ea05fdf82141dd
Ernqi <S10243067@connect.np.edu.sg>
2023-07-27 12:08:06 +0800

    Fixed Timers constrains

--- app/src/main/java/sg/edu/np/mad/team5MADAssignmentOnTask/TaskAnalysis.java
+++ app/src/main/java/sg/edu/np/mad/team5MADAssignmentOnTask/TaskAnalysis.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
